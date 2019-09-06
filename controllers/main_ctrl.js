763176e4b3564fd5e748ac6614451c14d1e7775a
Octavio Duarte <octavio.duarte@euemdia.com.br>
2019-09-06 11:45:56 -0300

    App Structure

--- controllers/main_ctrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = () => {
-    return {
-        routeSimpleResponse: (req, res) => {
-            res.status(200).send({ name: "Octávio Duarte" });
-        }
-    };
-};
